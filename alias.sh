3fd4a1e380a60d69b7459e227d807120d4606de1
Heesang Chae <theand@gmail.com>
2014-07-20 16:07:33 +0900

    fix alias.sh

--- alias.sh
+++ alias.sh
@@ -1,34 +1,62 @@
-function get_all_git_repo(){
+
+#################
+
+#서브디렉토리들의 git remote 저장소 주소를 뽑아내기.
+function get_all_git_origin(){
     for DIR in *;
     do
         [[ -d $DIR ]] && [[ -d $DIR/.git ]] && cd $DIR && git config --get remote.origin.url && cd .. ;
     done
 }
 
+#지정한 디렉토리 목록에 대해서만 저장소 주소를 뽑아냄
+function get_git_origin_from_txt(){
+    while read line;
+    do
+        cd $line ;
+        git config --get remote.origin.url ;
+        cd ..;
+    done < $1
+}
+
+
+#git 저장소 경로를 담고 있는 txt파일을 읽어서 프로젝트를 모두 clone.
+function clone_repo_from_txt(){
+    while read line;
+    do
+        git clone $line ;
+    done < $1
+}
+
+#################
+
+#서브디렉토리의 모든 git 저장소를 업데이트.
 function update_git_all(){
-    for DIR in *; 
+    for DIR in *;
     do
         echo "Processing : $DIR/"
-        [[ -d $DIR ]] && [[ -d $DIR/.git ]] && cd $DIR && git pull && cd .. ; 
+        [[ -d $DIR ]] && [[ -d $DIR/.git ]] && cd $DIR && git pull && cd .. ;
     done
 }
+
 
 function works_update_all(){
-    pushd .
-    cd ~/Works/Angular-Case-Study;
-    update_git_all;
-    cd ~/Works/My-Study-Material;
-    update_git_all;
-    cd ~/Works/Laravel-Case-Study;
-    update_git_all;
-    popd
-}
-
-function update_composer_all(){
-    for DIR in *; 
+    cd ~/Works/case-study-repo-list
+    #cd /var/www
+    for DIR in *.txt;
     do
-        echo "Processing : $DIR/"
-        [[ -d $DIR ]] && [[ -e $DIR/composer.json ]]  && cd $DIR && composer update && cd .. ; 
+        bname=$(basename "$DIR" .txt)
+        echo "Case Study : $bname"
+        [[ -d ../$bname ]] && cd ../$bname && update_git_all && cd ../case-study-repo-list;
     done
 }
 
+#################
+
+function update_composer_all(){
+    for DIR in *;
+    do
+        echo "Processing : $DIR/"
+        [[ -d $DIR ]] && [[ -e $DIR/composer.json ]]  && cd $DIR && composer update && cd .. ;
+    done
+}
