d9e49703677db6c09dc01e25705d4616b03834e5
Heesang Chae <theand@gmail.com>
2016-01-10 22:26:03 +0900

    update script

--- alias.sh
+++ alias.sh
@@ -61,14 +61,14 @@ function update_git_all(){
 
 function works_update_all(){
     cd ~/Works/case-study-repo-list;
-    for DIR in *-*.txt;
+    for DIR in *.txt;
     do
         bname=$(basename "$DIR" .txt);
         echo "==================="
         echo "Case Study : $bname"
         echo "==================="
 
-        if [[ -d ../$bname ]] && [[ $bname != "TutsPlus-Resources" ]] && [[ $bname != "My-Module-Work" ]] && [[ $bname != "My-Sample-Code" ]] && [[ $bname != "My-Study-Material" ]]  
+        if [[ -d ../$bname ]] && [[ $bname != "TutsPlus-Resources" ]] && [[ $bname != "My-Module-Work" ]] && [[ $bname != "My-Sample-Code" ]] && [[ $bname != "My-Study-Material" ]] && [[ $bname != "boxen" ]]
         then
             cd ../$bname ;
             update_git_all ;
@@ -113,8 +113,26 @@ function create_txt_for_all_casestudy(){
     cd ../Angular-Case-Study/
     get_all_git_origin > ../case-study-repo-list/angular_new.txt
 
+    cd ../Atom-Case-Study/
+    get_all_git_origin > ../case-study-repo-list/atom_new.txt
+
     cd ../Bot-Case-Study/
     get_all_git_origin > ../case-study-repo-list/bot_new.txt
+
+    cd ../boxen/
+    get_all_git_origin > ../case-study-repo-list/boxen_new.txt
+
+    cd ../Chrome-Case-Study/
+    get_all_git_origin > ../case-study-repo-list/chrome_new.txt
+
+    cd ../Django-Case-Study/
+    get_all_git_origin > ../case-study-repo-list/django_new.txt
+
+    cd ../Electron-Case-Study/
+    get_all_git_origin > ../case-study-repo-list/electron_new.txt
+
+    cd ../Flask-Case-Study/
+    get_all_git_origin > ../case-study-repo-list/flask_new.txt
 
     cd ../FrontEnd-Case-Study/
     get_all_git_origin > ../case-study-repo-list/front_new.txt
@@ -122,23 +140,11 @@ function create_txt_for_all_casestudy(){
     cd ../General-Case-Study/
     get_all_git_origin > ../case-study-repo-list/general_new.txt
 
-    cd ../Java-Case-Study/
-    get_all_git_origin > ../case-study-repo-list/java_new.txt
-
-    cd ../Spring-Case-Study/
-    get_all_git_origin > ../case-study-repo-list/spring_new.txt
-
-    cd ../SpringGuides-Case-Study/
-    get_all_git_origin > ../case-study-repo-list/sg_new.txt
-
     cd ../JS-Case-Study/
     get_all_git_origin > ../case-study-repo-list/js_new.txt
 
-    cd ../Electron-Case-Study/
-    get_all_git_origin > ../case-study-repo-list/electron_new.txt
-
-    cd ../NodeJS-Case-Study/
-    get_all_git_origin > ../case-study-repo-list/nodejs_new.txt
+    cd ../Java-Case-Study/
+    get_all_git_origin > ../case-study-repo-list/java_new.txt
 
     cd ../Laracast/
     get_all_git_origin > ../case-study-repo-list/laracast_new.txt
@@ -167,20 +173,29 @@ function create_txt_for_all_casestudy(){
     cd ../My-Translation-Work/
     get_all_git_origin > ../case-study-repo-list/trans_new.txt
 
+    cd ../NodeJS-Case-Study/
+    get_all_git_origin > ../case-study-repo-list/nodejs_new.txt
+
     cd ../PHP-Case-Study/
     get_all_git_origin > ../case-study-repo-list/php_new.txt
 
     cd ../Python-Case-Study/
     get_all_git_origin > ../case-study-repo-list/python_new.txt
 
-    cd ../Ruby-Case-Study/
-    get_all_git_origin > ../case-study-repo-list/ruby_new.txt
+    cd ../Rails-Case-Study/
+    get_all_git_origin > ../case-study-repo-list/rails_new.txt
 
     cd ../React-Case-Study/
     get_all_git_origin > ../case-study-repo-list/react_new.txt
 
-    cd ../Rails-Case-Study/
-    get_all_git_origin > ../case-study-repo-list/rails_new.txt
+    cd ../Ruby-Case-Study/
+    get_all_git_origin > ../case-study-repo-list/ruby_new.txt
+
+    cd ../Spring-Case-Study/
+    get_all_git_origin > ../case-study-repo-list/spring_new.txt
+
+    cd ../SpringGuides-Case-Study/
+    get_all_git_origin > ../case-study-repo-list/sg_new.txt
 
     cd ../Sinatra-Case-Study/
     get_all_git_origin > ../case-study-repo-list/sinatra_new.txt
@@ -188,11 +203,11 @@ function create_txt_for_all_casestudy(){
     cd ../StaticSite-Case-Study/
     get_all_git_origin > ../case-study-repo-list/static_new.txt
 
-    cd ../TutsPlus-Resources/
-    get_all_git_origin > ../case-study-repo-list/tuts_new.txt
-
     cd ../TIL/
     get_all_git_origin > ../case-study-repo-list/til_new.txt
+
+    cd ../TutsPlus-Resources/
+    get_all_git_origin > ../case-study-repo-list/tuts_new.txt
 
     cd ..
 
@@ -212,8 +227,23 @@ function clone_repo_for_all_casestudy(){
     cd ../Angular-Case-Study/
     clone_repo_from_txt ../case-study-repo-list/Angular-Case-Study.txt
 
-    cd Bot-Case-Study/
+    cd ../Atom-Case-Study/
+    clone_repo_from_txt ../case-study-repo-list/Atom-Case-Study.txt
+
+    cd ../Bot-Case-Study/
     clone_repo_from_txt ../case-study-repo-list/Bot-Case-Study.txt
+
+    cd ../Chrome-Case-Study/
+    clone_repo_from_txt ../case-study-repo-list/Chrome-Case-Study.txt
+
+    cd ../Django-Case-Study/
+    clone_repo_from_txt ../case-study-repo-list/Django-Case-Study.txt
+
+    cd ../Electron-Case-Study/
+    clone_repo_from_txt ../case-study-repo-list/Electron-Case-Study.txt
+
+    cd ../Flask-Case-Study/
+    clone_repo_from_txt ../case-study-repo-list/Flask-Case-Study.txt
 
     cd ../FrontEnd-Case-Study/
     clone_repo_from_txt ../case-study-repo-list/FrontEnd-Case-Study.txt
@@ -221,23 +251,11 @@ function clone_repo_for_all_casestudy(){
     cd ../General-Case-Study/
     clone_repo_from_txt ../case-study-repo-list/General-Case-Study.txt
 
-    cd ../Java-Case-Study/
-    clone_repo_from_txt ../case-study-repo-list/Java-Case-Study.txt
-
-    cd ../Spring-Case-Study/
-    clone_repo_from_txt ../case-study-repo-list/Spring-Case-Study.txt
-
-    cd ../SpringGuides-Case-Study/
-    clone_repo_from_txt ../case-study-repo-list/SpringGuides-Case-Study.txt
-
     cd ../JS-Case-Study/
     clone_repo_from_txt ../case-study-repo-list/JS-Case-Study.txt
 
-    cd ../NodeJS-Case-Study/
-    clone_repo_from_txt ../case-study-repo-list/NodeJS-Case-Study.txt
-
-    cd ../Electron-Case-Study/
-    clone_repo_from_txt ../case-study-repo-list/Electron-Case-Study.txt
+    cd ../Java-Case-Study/
+    clone_repo_from_txt ../case-study-repo-list/Java-Case-Study.txt
 
     cd ../Laracast/
     clone_repo_from_txt ../case-study-repo-list/Laracast.txt
@@ -251,6 +269,9 @@ function clone_repo_for_all_casestudy(){
     cd ../LetsCodeJavaScript-LessonLearned/
     clone_repo_from_txt ../case-study-repo-list/LetsCodeJavaScript-LessonLearned.txt
 
+    cd ../Motion-Case-Study/
+    clone_repo_from_txt ../case-study-repo-list/Motion-Case-Study.txt
+
     cd ../My-Module-Work/
     clone_repo_from_txt ../case-study-repo-list/My-Module-Work.txt
 
@@ -263,32 +284,41 @@ function clone_repo_for_all_casestudy(){
     cd ../My-Translation-Work/
     clone_repo_from_txt ../case-study-repo-list/My-Translation-Work.txt
 
+    cd ../NodeJS-Case-Study/
+    clone_repo_from_txt ../case-study-repo-list/NodeJS-Case-Study.txt
+
     cd ../PHP-Case-Study/
     clone_repo_from_txt ../case-study-repo-list/PHP-Case-Study.txt
 
     cd ../Python-Case-Study/
     clone_repo_from_txt ../case-study-repo-list/Python-Case-Study.txt
 
-    cd ../Ruby-Case-Study/
-    clone_repo_from_txt ../case-study-repo-list/Ruby-Case-Study.txt
+    cd ../Rails-Case-Study/
+    clone_repo_from_txt ../case-study-repo-list/Rails-Case-Study.txt
 
     cd ../React-Case-Study/
     clone_repo_from_txt ../case-study-repo-list/React-Case-Study.txt
 
-    cd ../Rails-Case-Study/
-    clone_repo_from_txt ../case-study-repo-list/Rails-Case-Study.txt
+    cd ../Ruby-Case-Study/
+    clone_repo_from_txt ../case-study-repo-list/Ruby-Case-Study.txt
 
     cd ../Sinatra-Case-Study/
     clone_repo_from_txt ../case-study-repo-list/Sinatra-Case-Study.txt
 
+    cd ../Spring-Case-Study/
+    clone_repo_from_txt ../case-study-repo-list/Spring-Case-Study.txt
+
+    cd ../SpringGuides-Case-Study/
+    clone_repo_from_txt ../case-study-repo-list/SpringGuides-Case-Study.txt
+
     cd ../StaticSite-Case-Study/
     clone_repo_from_txt ../case-study-repo-list/StaticSite-Case-Study.txt
 
-    cd ../TutsPlus-Resources/
-    clone_repo_from_txt ../case-study-repo-list/TutsPlus-Resources.txt
-
     cd ../TIL/
     clone_repo_from_txt ../case-study-repo-list/TIL.txt
+
+    cd ../TutsPlus-Resources/
+    clone_repo_from_txt ../case-study-repo-list/TutsPlus-Resources.txt
 
     cd ..
 
